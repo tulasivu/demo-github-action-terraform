5e8ffc9677f70ba1d6e561afd05d80284d1bf391
tulasivu <tulasivuyyuru@gmail.com>
2024-06-19 10:49:07 -0400

    added args to pass instance_type

--- main.tf
+++ main.tf
@@ -16,9 +16,5 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+  instance_type = var.instance_type
 }
